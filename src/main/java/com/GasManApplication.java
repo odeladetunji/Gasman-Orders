cd0b1f4d2f08a828de0867104f3c6c2f73a8789f
Olatunji Odelade <odeladetunji@gmail.com>
2019-10-31 03:58:19 +0100

    bootstraping

---
+++ src/main/java/com/GasManApplication.java
@@ -0,0 +1,48 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasManApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GasManApplication.class, args);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
